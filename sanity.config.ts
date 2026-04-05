af1bcbadd17ad325b343257e76c32343170159f7
sophie-goodman <sgsg100@live.com>
2026-04-04 23:27:27 -0400

    sanity config

--- sanity.config.ts
+++ sanity.config.ts
@@ -15,8 +15,8 @@ import {structure} from './sanity/structure'
 
 export default defineConfig({
   basePath: '/studio',
-  projectId,
-  dataset,
+  projectId:'huz1pj9w',
+  dataset:'production',
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
   plugins: [
